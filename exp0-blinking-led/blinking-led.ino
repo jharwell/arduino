c68b727926e5e8178988f68c2c948c867931df48
John Harwell <john.r.harwell@gmail.com>
2018-10-15 14:56:14 -0500

    Add exp0: blinking LED

---
+++ exp0-blinking-led/blinking-led.ino
@@ -0,0 +1,49 @@
+/**
+ * @file blinking-led.ino
+ *
+ * This is free and unencumbered software released into the public domain.
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ */
+
+/*
+ * The pin # of the LED we are going to use.
+ */
+#define LED_PIN 13
+
+/*
+ * 1-time setup code that runs during initialization.
+ */
+void setup(void) {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+/*
+ * Main code, which will run forever.
+ */
+void loopvoid() {
+digitalWrite(LED_PIN, HIGH);
+delay(1000);
+digitalWrite(LED_PIN, LOW);
+delay(1000);
+}
