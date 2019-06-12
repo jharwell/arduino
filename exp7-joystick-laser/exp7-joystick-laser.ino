9a539dfba9da99379e232de6152e3ee848f56a35
John Harwell <john.r.harwell@gmail.com>
2019-06-12 10:27:27 -0500

    Update exp7: joystick laser, for summer 2019 coding academy

--- exp7-joystick-laser/exp7-joystick-laser.ino
+++ exp7-joystick-laser/exp7-joystick-laser.ino
@@ -1,5 +1,5 @@
 /**
- * @file exp6-bagels.ino
+ * @file exp7-joystick-laser.ino
  *
  * This is free and unencumbered software released into the public domain.
  * Anyone is free to copy, modify, publish, use, compile, sell, or
@@ -34,14 +34,16 @@
 /*******************************************************************************
  * Constant Definitions
  ******************************************************************************/
-#define XSERVO_PIN 10 /* Servo pin that will control X motion */
-#define YSERVO_PIN 9 /* Servo pin that will control Y motion */
+#define XSERVO_PIN 10 /* Servo pin that will control X motion (yaw) */
+#define YSERVO_PIN 9 /* Servo pin that will control Y motion (pitch) */
 
-#define LASER_PIN 6 /* pin connected to laser output */
-#define LASER_BUTTON_PIN 7 /* pin for button to toggle laser on/off */
+#define LASER_PIN 7        /* Pin connected to laser output */
+#define LASER_BUTTON_PIN 6 /* Pin for button to toggle laser on/off */
 
-#define XPOS_PIN A0 /* select the input pin for the joystick X potentiometer */
-#define YPOS_PIN A1 /* select the input pin for the joystick Y potentiometer */
+#define XPOS_PIN A0 /* Input pin for the joystick X potentiometer */
+#define YPOS_PIN A1 /*  Input pin for the joystick Y potentiometer */
+
+#define DEBOUNCE_ENABLE 1 /* Should we debounce button presses? */
 
 /*******************************************************************************
  * Global Variables
@@ -52,77 +54,95 @@ int xpos = 0;
 int ypos = 0;
 
 /* laser variables */
-int laser_on = 0;
-int button_press = 0;
 unsigned long last_debounce_time = 0;
+int laser_button_state = HIGH;
+int laser_on = LOW;
 
 /*******************************************************************************
  * Functions
  ******************************************************************************/
-
 /**
- * @brief Perform debouncing of the button that controls the laser.
- *
- * @param last_debounce The last time (in milliseconds) that we checked the
- *                      button value to see if it has debounced yet.
- * @param last_reading The previous reading of the button.
- *
- * @return 1 if the button has been debounced, 0 otherwise.
+ * Setup function: runs once when the sketch is loaded/Arduino is powered on
  */
-int button_debounce(int last_debounce, int last_reading) {
-  unsigned long debounce_delay = 50;
-
-  int reading = digitalRead(LASER_BUTTON_PIN);
-
-  /* If the switch changed, due to noise or pressing reset the debounce timer */
-  if (reading != last_reading) {
-    last_debounce = millis();
-  }
-
-  /*
-   * Whatever the reading is at, it's been there for longer than the debounce
-   * delay, so take it as the actual current state
-   */
-  if ((millis() - last_debounce) >= debounce_delay && reading == last_reading) {
-    return 1;
-  }
-  return 0;
-} /* button_debounce() */
-
 void setup(void) {
   pinMode(LASER_PIN,OUTPUT);
   pinMode(LASER_BUTTON_PIN,INPUT);
   digitalWrite(LASER_BUTTON_PIN,HIGH);  /* Enable the Arduino pullup for this pin */
-
+  Serial.begin(9600);
   xservo.attach(XSERVO_PIN);
   yservo.attach(YSERVO_PIN);
 } /* setup() */
 
+
+/**
+ * Loop function: runs over and over infinitely after the setup() function
+ * finishes until the loaded sketch is changed or the Arduino is powered off.
+ */
 void loop(void) {
-  /*
-   * The button press is active low , so if it is pressed we will flip the laser
-   * state from on -> off or off -> on. To read the button signal we can:
-   *
-   * 1. Read the raw value from the button, and use as is (possibly noisy).
-   * 2. We can "debounce" the button to prevent it momentary noises/presses from
-   * erroneously tripping the laser.
-   */
-  button_press = digitalRead(LASER_BUTTON_PIN);
-  int pressed = button_debounce(last_debounce_time, button_press);
 
-  if ((button_press == LOW) && pressed) {
-    laser_on = !laser_on;
-  }
-  digitalWrite(LASER_PIN, laser_on); //Turn Laser on
+  /* Do everything related to the laser we need to do */
+  handle_laser(DEBOUNCE_ENABLE);
 
-  xpos = analogRead(XPOS_PIN);     // read and store the x position location of the joystick
+  /* Do everything related to reading joystick and moving the servos we need to do */
+  handle_servos();
 
-  xpos = map(xpos,0,1023,0,180);  //map the analog read x values to the 0-180 servo values
-  ypos = analogRead(YPOS_PIN);     // read and store the y position of the joystick
-
-  ypos = map(ypos,0,1023,0,180);  //map the analog read y values to the 0-180 servo values
-
-  xservo.write(xpos);             // move the X Location servo to the x position the joystick is at
-  yservo.write(ypos);             // move the Y Location servo to the y position the joystick is at
-  delay(15);
+  /* Wait a 15ms before running the loop again */
+  delay(100);
 } /* loop() */
+
+void handle_servos() {
+  /*
+   * Read the analog x position location of the joystick and map
+   * the value (which is 0-1023) to the 0-180 range for the servo.
+   */
+  xpos = analogRead(XPOS_PIN);
+  xpos = map(xpos, 0, 1023, 0, 180);
+  /*
+   * Read the analog Y position location of the joystick and map
+   * the value (which is 0-1023) to the 0-180 range for the servo.
+   */
+  ypos = analogRead(YPOS_PIN);
+  ypos = map(ypos, 0, 1023, 0, 180);
+  /* move the yaw servo to the X position the joystick is at */
+  xservo.write(xpos);
+
+  /* move the pitch servo to the Y position the joystick is at */
+  yservo.write(ypos);
+} /* handle_servos() */
+
+/**
+ * Perform debouncing of the button that controls the laser and toggle the laser
+ * appropriately.
+ *
+ * The button press is active low , so if it is pressed we will flip the laser
+ * state from on -> off or off -> on. To read the button signal we can:
+ *
+ * 1. Read the raw value from the button, and use as is (possibly noisy).
+ * 2. We can "debounce" the button to prevent it momentary noises/presses from
+ * erroneously tripping the laser.
+ */
+int handle_laser(int debounce_enable) {
+  unsigned long debounce_delay = 100;
+
+  /* sample the state of the button--is it pressed or not? */
+  int reading = digitalRead(LASER_BUTTON_PIN);
+
+  /* filter out any noise by setting a timer */
+  if ((debounce_enable && (millis() - last_debounce_time) > debounce_delay) ||
+      !debounce_enable) {
+
+    /* If the button is actually pressed, turn on the laser if it was off  */
+    if (reading == LOW && laser_on == LOW) {
+      digitalWrite(LASER_PIN, HIGH);
+      laser_on = HIGH;
+      last_debounce_time = millis();
+    }
+    /* If the button is actually pressed, turn off the laser if it was on  */
+    else if (reading == LOW && laser_on == HIGH) {
+      digitalWrite(LASER_PIN, LOW);
+      laser_on = LOW;
+      last_debounce_time = millis();
+    }
+  }
+
+} /* handle_laser() */
